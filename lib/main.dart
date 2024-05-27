654fab2acad1a3348cfe719073c0bfae15de204d
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-27 07:03:28 +0530

    design signup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/page/signin.dart';
+import 'package:chatapp/page/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignIn(),
+      home: const SignUp(),
     );
   }
 }
