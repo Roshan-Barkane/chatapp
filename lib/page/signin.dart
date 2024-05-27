167dd0b0c451df6d1d2e9185bbb64fba43a3a106
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-27 06:37:17 +0530

    design signin page

--- lib/page/signin.dart
+++ lib/page/signin.dart
@@ -10,6 +10,203 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 3.5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF7f30fe),
+                      Color(0xFF6380fb),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.vertical(
+                      bottom: Radius.elliptical(
+                          MediaQuery.of(context).size.width, 105.0))),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 70.0),
+              child: Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "SignIn ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Login to your account",
+                      style: TextStyle(
+                          color: Color(0xFFbbb0ff),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                    child: Material(
+                      elevation: 3.5,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 20.0),
+                        height: MediaQuery.of(context).size.height / 2,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              "Email",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 10.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1.0, color: Colors.black38),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(
+                                    Icons.mail_outline,
+                                    color: Color(0xFF7f30fe),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              "Password",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 10.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1.0, color: Colors.black38),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(
+                                    Icons.password,
+                                    color: Color(0xFF7f30fe),
+                                  ),
+                                ),
+                                obscureText: true,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                "Forget Possword ?",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 40.0,
+                            ),
+                            Center(
+                              child: Container(
+                                width: 130,
+                                child: Material(
+                                  elevation: 5.0,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Container(
+                                    padding: EdgeInsets.all(10.0),
+                                    decoration: BoxDecoration(
+                                      color: Color(0xFF6380fb),
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "SignIn",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.black, fontSize: 16.0),
+                      ),
+                      Text(
+                        " Sign Up Now!",
+                        style: TextStyle(
+                          color: Color(0xFF7f30fe),
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
